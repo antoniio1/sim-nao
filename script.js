5c62bacda25984ff03945e50c1e3693a4d0b120e
antoniio1 <137443376+antoniio1@users.noreply.github.com>
2024-02-20 21:13:30 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('sim').addEventListener('click', function() {
+    alert('Você escolheu Sim! Ebaaa');
+});
+
+document.getElementById('nao').addEventListener('click', function() {
+    alert('Você escolheu Não! Que pena :(');
+});
+
+console.log("O usuário escolheu:", resposta); {
+    
+}
